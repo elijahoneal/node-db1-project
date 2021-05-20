73d216549f86e9ce748f2a75324eae75f4a63d42
Elijah <elijahoneal@yahoo.com>
2021-05-19 23:11:07 -0400

    Account Router

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,35 @@
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const account = req.body
+  if(!account.name || !account.budget) {
+    res.status(400).json({ message: "name and budget are required" })
+  } else if(typeof account.name != 'string' ){
+    res.json({ message: "name of account must be a string" })
+  } else if(account.name.trim() < 3 || account.name.trim() > 100){
+    res.json({ message: "name of account must be between 3 and 100" })
+  } else if(typeof account.budget != 'number'){
+    res.json({ message: "budget of account must be a number" })
+  } else if(account.budget < 0 || account.budget > 1000000){
+    res.json({ message: "budget of account is too large or too small" })
+  } else {
+    next()
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  if(req.body.name.trim()){
+    res.status(400).json({ message: "that name is taken" })
+  } else {
+    next()
+  }
 }
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  if(!req.params.id){
+    res.status(404).json({ message: "account not found" })
+  } else {
+    next()
+  }
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,46 @@
 const router = require('express').Router()
-
+const Accounts = require('./accounts-model')
+const { checkAccountPayload , checkAccountId, checkAccountNameUnique } = require('./accounts-middleware')
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.getAll()
+  .then( allAccounts => res.status(200).json(allAccounts) )
+  .catch(next(err))
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.getById(req.params.id)
+  .then( account => res.status(200).json(account) )
+  .catch(next(err))
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.create(req.body)
+  .then(newAccount => res.status(201).json(newAccount))
+  .catch(next(err))
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.updateById(req.params.id , req.body)
+  .then( () => {
+    return Accounts.getById(req.params.id)
+  } )
+  .catch(next(err))
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.deleteById(req.params.id)
+  .then(account => res.status(200).json(account))
+  .catch(next(err))
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status( err.status || 500 ).json({message: 'Something went wrong'})
 })
 
 module.exports = router;
