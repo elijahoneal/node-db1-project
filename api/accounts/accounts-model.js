dfc6b6a8c0af035cbf5d3c086032350905da48ed
Elijah <elijahoneal@yahoo.com>
2021-05-19 22:18:37 -0400

    Model Functions

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,34 @@
+const { del } = require('../../data/db-config')
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('accounts')
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts').where({id}).first()
 }
 
-const create = account => {
+async const create = account => {
   // DO YOUR MAGIC
+  const [id] = await db('accounts').insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
+async const updateById = (id, account) => {
   // DO YOUR MAGIC
+await db('accounts').where({id}).update(account)
+return getById(id)
 }
 
-const deleteById = id => {
+async const deleteById = id => {
   // DO YOUR MAGIC
+  const deleted = await getById(id)
+
+  await db('accounts').where('id',id).delete();
+  return deleted
 }
 
 module.exports = {
